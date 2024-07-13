3ff00590911d30ad8e772ff95f4add3d423fc98d
Sergey <one6sergey@gmail.com>
2024-07-13 17:27:32 +0300

    add search

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import viewsets
 from profiles_api import serializers, models, permissions
-
+from rest_framework import filters
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
@@ -12,3 +12,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     authentication_classes = [TokenAuthentication,]
     permission_classes = [permissions.UpdateOwnProfile,]
+    filter_backends = [filters.SearchFilter,]
+    search_fields = ['name', 'email',]
